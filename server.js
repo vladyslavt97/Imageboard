57c4d47c28c435ed4db283c6d62cd631d20ff63f
Yair <32864499+bluepostit@users.noreply.github.com>
2022-10-11 14:49:53 +0200

    give default port value

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 require('dotenv').config();
-const { PORT } = process.env;
+const { PORT = 8080 } = process.env;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
