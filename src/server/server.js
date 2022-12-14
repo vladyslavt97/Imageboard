53e752bd3e885ca375777620e8a556d92b7c7558
Martin Zachoszcz <zartin@MacBook-Pro.local>
2022-12-14 17:34:17 +0100

    feat: update folder structure and update multer to avoid security issues

--- src/server/server.js
+++ src/server/server.js
@@ -4,11 +4,11 @@ const app = express();
 require('dotenv').config();
 const { PORT = 8080 } = process.env;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "..", "client")));
 app.use(express.json());
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`I'm listening on port ${PORT}`));
